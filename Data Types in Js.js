356460afb8338e4f36cfb459c9316a82b37bb9ee
Ritesh Ranjan <51942843+riteshranjan1997@users.noreply.github.com>
2019-09-06 22:33:47 +0530

    Add files via upload

---
+++ Data Types in Js.js
@@ -0,0 +1,48 @@
+/* Data Types:
+undefined, null, boolean, string, symbol, number, and object
+*/
+
+//  we can first declare a variable and  we can assign value afterwords 
+
+var a;
+
+console.log(a)
+
+a = 7;
+//here we are declearing and  assigning both in one line
+
+var b = 2;
+
+b = a;
+
+console.log(a)
+
+//note: it is ok to not give ; at the end of the line 
+
+// we can assign any type of data in variable like
+
+ var my name = 'Ritesh' // String Data Type
+
+ let number = 8 // Number Data Type
+
+// Initialize these three variables
+
+var x;
+var y;
+var z;
+
+// Assigning the value to them
+
+x = 17;
+y = "I am a String"
+z = 7.5 // it is called floating number 
+
+// variables name in javascript is case Sensitive Example
+
+var firST;
+
+first = 10; // 10 will not be assigned in firST
+console.log(firST)
+
+firST = 10;
+console.log(firST)
