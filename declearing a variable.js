36dfe57a0d14a0035aa458dbd9195f81aad7ffe5
Ritesh Ranjan <51942843+riteshranjan1997@users.noreply.github.com>
2019-09-06 22:14:47 +0530

    Add files via upload

---
+++ declearing a variable.js
@@ -0,0 +1,7 @@
+// first way to declare a variable is "var"
+var myvariable = 5;
+// second way to declare a variable is "let"
+let myvariable = 6;
+// constent are also declared in JavaScript
+// const value is not change throughout the program
+const pi = 3.14
