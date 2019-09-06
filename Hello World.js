e8345888e4bcfbfd90fdb9cac91a9ba1b2d25b96
Ritesh Ranjan <51942843+riteshranjan1997@users.noreply.github.com>
2019-09-06 22:12:36 +0530

    Update Hello World.js

--- Hello World.js
+++ Hello World.js
@@ -1,6 +1,6 @@
 // this is used to comment out the lines in javaScript
 // this is use to discribe the code in informal way
-/* this is also used to add comments to the code
+/* this is also used to add comments to the code in more then one line
 we have to end this comment */
 //we use console.log to give the run command
 console.log("Hello from JavaScript");
