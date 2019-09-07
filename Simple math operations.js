24072dec42b027ce36531dfd3fc808e496e2f86a
Ritesh Ranjan <51942843+riteshranjan1997@users.noreply.github.com>
2019-09-07 13:35:19 +0530

    Add files via upload

---
+++ Simple math operations.js
@@ -0,0 +1,59 @@
+// adding numbers
+var sum = 10 + 10;
+console.log(sum)
+
+// substracting numbers
+var difference = 45 - 3;
+console.log(difference)
+
+// multiplying numbers
+var product = 5 * 5;
+console.log(product)
+
+// dividing numbers
+var quotient = 90 / 10;
+console.log(quotient)
+
+// find remainder
+var remainder;
+remainder = 11 % 3;
+console.log(remainder)
+
+// Incrementing numbers
+var myVar = 50;
+myVar = myVar + 1;
+//or
+myVar++
+
+// decrementing numbers
+var myVar = 50;
+myVar = myVar - 1;
+//or
+myVar--
+
+// Note
+var a = 3;
+var b = 17;
+var c = 12;
+
+a = a + 12;
+b = 9 + b;
+c = c + 7;
+
+// other way to do this 
+
+a += 12;
+b += 9;
+c += 7
+
+a -= 6;
+b -= 15;
+c -= 1;
+
+a *= 8;
+b *= 3;
+c *= 1;
+
+a /= 10;
+b /=5;
+c /=1;
